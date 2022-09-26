aa857857e2771c8cbf60804a9bc62a4c5c59a7e3
amigin <amigin@gmail.com>
2022-09-26 13:28:36 +0300

    Made reading settings more efficient

--- src/fn_impl_settings_reader/generate.rs
+++ src/fn_impl_settings_reader/generate.rs
@@ -12,15 +12,18 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     result.push_str(" {\n");
 
     result.push_str(
-        r#"pub async fn load(file_name: &str) -> Self {{
-        if let Some(result) = Self::read_from_file(file_name) {{
-            return result;
-        }}
+        r#"pub async fn load(file_name: &str) -> Self {
+            match Self::read_from_file(file_name.to_string()).await {
+                Ok(settings) => return settings,
+                Err(_) => {
+                    println!("Unable to read settings from file: {}.", file_name);
+                }
+            }
     
-        Self::read_from_url().await
-    }}
+            Self::read_from_url().await
+        }
     
-    fn read_from_file(file_name: &str) -> Option<Self> {{
+    async fn read_from_file(file_name: &str) -> Option<Self> {
         let home = format!("{}"#,
     );
 
@@ -34,35 +37,33 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     result.push_str(
         r#"{}", std::env::var("HOME").unwrap(), file_name);
 
-
-    
-        let mut file_result = std::fs::File::open(home.as_str());
-    
-        if file_result.is_err() {{
-            println!("Can not read settings from file: {}", home);
-            return None;
-        }}
-    
+        let file_result = tokio::fs::File::open(file_name.as_str()).await; // Here
+        if file_result.is_err() {
+            return Err(format!("Can not read settings from file: {}", file_name));
+        }
         let mut result = Vec::new();
-        std::io::Read::read_to_end(&mut file_result.unwrap(), &mut result).unwrap();
-        Some(serde_yaml::from_slice(&result).unwrap())
-    }}
+        match tokio::io::AsyncReadExt::read_to_end(&mut file_result.unwrap(), &mut result).await {
+            Ok(_) => match serde_yaml::from_slice(&result) {
+                Ok(result) => Ok(result),
+                Err(err) => Err(format!(
+                    "Invalid yaml format of file: {}. Err: {}",
+                    file_name, err
+                )),
+            },
+            Err(_) => Err(format!("Can not read settings from file: {}", file_name)),
+        }
+    }
     
-    async fn read_from_url() -> Self {{
-        let url = std::env::var("SETTINGS_URL");
-    
-        if url.is_err() {{
-            panic!("Environmant variable SETTINGS_URL is not set");
-        }}
-    
-        let url = url.unwrap();
-    
-        let mut result = flurl::FlUrl::new(url.as_str(), None).get().await.unwrap();
-    
-        let body = result.get_body().await.unwrap();
-    
-        serde_yaml::from_slice(body).unwrap()
-    }}"#,
+    async fn read_from_url() -> Self {
+            let url = std::env::var("SETTINGS_URL");
+            if url.is_err() {
+                    panic!("Environmant variable SETTINGS_URL is not set");
+            }
+            let url = url.unwrap();
+            let mut result = flurl::FlUrl::new(url.as_str(), None).get().await.unwrap();
+            let body = result.get_body().await.unwrap();
+            serde_yaml::from_slice(body).unwrap()
+    }"#,
     );
 
     result.push_str("}\n");
@@ -84,48 +85,86 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     
     impl SettingsReader {
         pub async fn new(file_name: &str) -> Self {
+            if let Ok(settings) = "#,
+        );
+
+        result.push_str(struct_name.as_str());
+
+        result.push_str(
+            r#"::read_from_file(file_name.to_string()).await {
+                let settings = std::sync::Arc::new(tokio::sync::RwLock::new(settings));
+                tokio::spawn(update_settings_in_a_background(
+                    settings.clone(),
+                    Some(file_name.to_string()),
+                ));
+                return Self { settings };
+            }
             let settings = "#,
         );
 
         result.push_str(struct_name.as_str());
 
         result.push_str(
-            r#"::load(file_name).await;
-    
+            r#"::read_from_url().await;
             let settings = std::sync::Arc::new(tokio::sync::RwLock::new(settings));
-    
-            tokio::spawn(update_settings_in_a_background(settings.clone(),file_name.to_string(),));
-    
+            tokio::spawn(update_settings_in_a_background(settings.clone(), None));
             Self { settings }
         }
-    
         pub async fn get_settings(&self) -> "#,
         );
+
         result.push_str(struct_name.as_str());
+
         result.push_str(
-            r#"{
+            r#" {
             self.settings.read().await.clone()
         }
     }
     
-    async fn update_settings_in_a_background(settings: std::sync::Arc<tokio::sync::RwLock<"#,
+    async fn update_settings_in_a_background(
+        settings: std::sync::Arc<tokio::sync::RwLock<"#,
         );
+
         result.push_str(struct_name.as_str());
+
         result.push_str(
-            r#">>, file_name: String) {
+            r#">>,
+        file_name: Option<String>,
+    ) {
         loop {
             tokio::time::sleep(std::time::Duration::from_secs(60)).await;
-    
             let settings = settings.clone();
             let file_name = file_name.clone();
             let _ = tokio::spawn(async move {
-                let settings_model = "#,
+                let result = if let Some(file_name) = &file_name {
+                    let file_name = file_name.clone();
+                    match tokio::spawn("#,
         );
+
         result.push_str(struct_name.as_str());
+
         result.push_str(
-            r#"::load(file_name.as_str()).await;
-                let mut write_access = settings.write().await;
-                *write_access = settings_model;
+            r#"::read_from_file(file_name)).await {
+                        Ok(result) => result,
+                        Err(err) => Err(format!("Can not read settings from file. Err: {}", err)),
+                    }
+                } else {
+                    match tokio::spawn("#,
+        );
+
+        result.push_str(struct_name.as_str());
+
+        result.push_str(
+            r#"::read_from_url()).await {
+                        Ok(result) => Ok(result),
+                        Err(err) => Err(format!("Can not read settings from url. Err: {}", err)),
+                    }
+                };
+    
+                if let Ok(settings_model) = result {
+                    let mut write_access = settings.write().await;
+                    *write_access = settings_model;
+                }
             })
             .await;
         }
